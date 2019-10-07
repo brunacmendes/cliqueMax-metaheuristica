f8ac92109558d2f195a433401a318602be7a39fe
bruna <brumendes102@gmail.com>
2019-10-07 02:36:17 -0300

    07-10 heuristica finalizada

--- executa_instancias.py
+++ executa_instancias.py
@@ -18,32 +18,32 @@ files.append('benchmarks/C125.9.clq')
 files.append('benchmarks/C250.9.clq')
 files.append('benchmarks/C500.9.clq')
 files.append('benchmarks/C1000.9.clq')
-files.append('benchmarks/C2000.5.clq')
+files.append('benchmarks/C2000.9.clq')
 files.append('benchmarks/p_hat700-3.clq')
 files.append('benchmarks/p_hat700-2.clq')
 files.append('benchmarks/p_hat300-3.clq')
 files.append('benchmarks/p_hat300-2.clq')
 files.append('benchmarks/p_hat1500-2.clq')
+#files.append('benchmarks/small_test_set.txt')
 
 
-result = []
+
 size = len(files)
 heuristica = Heuristica()
 # roda heurística ITER vezes e salva resultados
-for x in range(0,size):
+for x in range(size):
     path = files[x]
-    
     result = heuristica.executa(path,ITER)
 
     content = 'Heuristica para Clique Maxima\n\n'
 
     #cria conteúdo 
-    content += 'INSTANCE\tITERATION\tMAXIMUM CLIQUE\tRUNTIME\n\n'    # MIN\tMAX\tMÉDIA\tMEDIANA\tVARIÂNCIA\tDESVIO\tQ1\tQ3\n\n
+    content += 'INSTANCE\tITERATION\tMAXIMUM_CLIQUE\tRUNTIME\n\n'    # MIN\tMAX\tMÉDIA\tMEDIANA\tVARIÂNCIA\tDESVIO\tQ1\tQ3\n\n
 
-    for i in range(0,ITER):
+    for i in range(ITER):
         content += '{}\t{}\t{}\t{}\n'.format(files[x], i+1, result[i][0], result[i][1])
         
-    print(content)
+    #print(content)
 
     with open(files[x]+"_results.txt", "w") as file:
         file.write(content)
