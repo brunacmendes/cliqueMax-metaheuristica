1b361633580fd386fa306e1159217c614aa1354c
bruna <brumendes102@gmail.com>
2019-11-29 03:04:40 -0300

    29/11

--- meta_heuristicas/TABU_metaheuristica.py
+++ meta_heuristicas/TABU_metaheuristica.py
@@ -16,3 +16,49 @@ Professor: Diego
 Disciplina: Meta-Heurística
 
 '''
+class TABU: #Variable Neighbourhood Search
+    def __init__(self): 
+        self.clique = [] #resultado do Max_Clique
+        self.instancia = Instancia()
+        self.tabu_list = []
+
+    def multistart(self, k, L, maxIter):
+        #clique inicial
+        S = random.choice(self.instancia.grafo)
+        Iter = 0
+        while Iter < maxIter:
+            S_best = self.TS(S,k, L, Iter)
+            #se S_best é um clique legal, retorna S e para
+
+            #else
+            #S_best = construcao de uma nova solucao
+
+
+        return -1 #failure
+
+    def TS(self, S, k, L, Iter):
+        I = 0
+        S_best = S
+        while I < L:
+
+            Iter = Iter + 1
+            if len(S) > len(S_best):
+                S_best = S
+                I = 0
+            else:
+                I = I + 1
+
+        
+
+        return S_best
+        
+
+    def executa(self, caminho, iteracoes):
+        self.result = []
+        self.instancia = Instancia()
+        self.instancia.le_benchmarks(caminho)
+        self.instancia.cria_o_grafo()
+        resposta = self.multistart(20, 100, iteracoes)
+        #print('MAX. CLIQUE: ' + str(len(self.clique)))
+
+        return resposta
